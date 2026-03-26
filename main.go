d75843c9bddd6efce42e8d9ded65ff2e161383a3
k20ku <heizhaokuan@gmail.com>
2026-03-26 11:55:48 +0900

    wip: considering multi line repl... huh...

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"os"
-	"os/user"
 	"path/filepath"
 	"strings"
+	"time"
 
 	liner "github.com/peterh/liner"
 )
@@ -15,63 +17,156 @@ var (
 	names         = []string{"alice", "bob", "carol", "duke"}
 )
 
-const PROMPT = ">> "
+const (
+	promptDefault  = ">> "
+	promptContinue = ".. "
+	indent         = "	"
+)
+
+type contLiner struct {
+	*liner.State
+	buffer string
+	depth  int
+}
+
+func newContLiner() *contLiner {
+	line := liner.NewLiner()
+	line.SetCtrlCAborts(true)
+
+	return &contLiner{State: line}
+}
+
+func (cl *contLiner) promptString() string {
+	// >> foo {
+	// .. <indent>
+	if cl.buffer != "" {
+		return promptContinue + strings.Repeat(indent, cl.depth)
+	}
+
+	return promptDefault
+}
+
+func (cl *contLiner) Prompt(prompt string) (string, error) {
+	line, err := cl.State.Prompt(cl.promptString())
+
+	switch err {
+	case nil:
+		if cl.buffer != "" {
+			cl.buffer = cl.buffer + "\n" + line
+		} else {
+			cl.buffer = line
+		}
+	case io.EOF:
+		// when ^D
+		if cl.buffer != "" {
+			// cancel line continuation if in continuation
+			cl.Accepted()
+		}
+		fmt.Println()
+		// else do nothing
+	case liner.ErrPromptAborted:
+		if cl.buffer != "" {
+			cl.Accepted()
+		} else {
+			fmt.Println("(^D to quit)")
+		}
+	}
+
+	return cl.buffer, err
+}
+
+func (cl *contLiner) Accepted() {
+	// cl.State.AppendHistory(cl.buffer)
+	cl.Clear()
+}
+
+func (cl *contLiner) Clear() {
+	cl.buffer = ""
+	cl.depth = 0
+}
+
+func (cl *contLiner) Close() {
+	cl.Clear()
+	cl.State.Close()
+}
+
+var errUnmatchedBraces = errors.New("unmatched braces")
+
+func (cl *contLiner) ReIndent() error {
+	oldDepth := cl.depth
+	cl.depth = cl.countDepth(cl.buffer)
+
+	if cl.depth < 0 {
+		return errUnmatchedBraces
+	}
+
+	if oldDepth < cl.countDepth(cl.buffer) {
+		lines := strings.Split(cl.buffer, "\n")
+		if len(lines) > 1 {
+			lastLine := lines[len(lines)-1]
+			cursorUp()
+			fmt.Printf("\r%s%s", cl.promptString(), lastLine) // ..
+			eraseInLine()
+			fmt.Println()
+		}
+	}
+
+	return nil
+}
+
+func (cl *contLiner) countDepth(src string) int {
+	depth := 0
+	for _, r := range src {
+		switch r {
+		case '{', '(':
+			depth++
+		case '}', ')':
+			depth--
+		}
+	}
+
+	return depth
+}
+
+func cursorUp() {
+	defer time.Sleep(1 * time.Second)
+	fmt.Print("\x1b[1A")
+}
+
+func cursorDown() {
+	defer time.Sleep(1 * time.Second)
+	fmt.Print("\x1b[1B")
+}
+
+func eraseInLine() {
+	defer time.Sleep(1 * time.Second)
+	fmt.Print("\x1b[0K")
+}
+
+func cursorToBeginThenDownBy(n int) {
+	defer time.Sleep(1 * time.Second)
+	fmt.Printf("\x1b[%dE", n)
+}
 
 func main() {
+	defer println()
 
-	line := liner.NewLiner()
-	defer func() {
-		if hfd, err := os.Create(history_fname); err == nil { // truncate history_file if it exists
-			line.WriteHistory(hfd)
-			hfd.Close()
-		} else {
-			fmt.Println("Error writing history file ", err)
-		}
-		line.Close()
-		println("Thank you. Goodbye!")
-	}()
-
-	// setting
-	line.SetMultiLineMode(true)
-	line.SetCtrlCAborts(true)
-	line.SetCompleter(func(line string) (c []string) {
-		for _, nm := range names {
-			if strings.HasPrefix(nm, strings.ToLower(line)) {
-				c = append(c, nm)
-			}
-		}
-		return
-	})
-
-	if hfd, err := os.Open(history_fname); err == nil {
-		line.ReadHistory(hfd)
-		hfd.Close()
-	}
-
-	if user, err := user.Current(); err == nil {
-		fmt.Printf("Hello %s! ", user.Username)
-	} else {
-		fmt.Print("Hello! ")
-	}
-
-	fmt.Println("This is the Monkey programming language!")
-	fmt.Println("Feel free to type in commands")
-
+	// contl := newContLiner()
+	// defer func() {
+	// 	contl.Close()
+	// 	println("Thank you. Goodbye!")
+	// }()
 	for {
-		if name, err := line.Prompt(PROMPT); err == nil {
+		fmt.Println("row1: ")
+		fmt.Println("row2: Good Morning!")
+		fmt.Println("row3: ")
 
-			if strings.HasPrefix(name, "/exit") {
-				break
-			}
-			fmt.Println("Got:", name)
-			line.AppendHistory(name)
-
-		} else if err == liner.ErrPromptAborted {
-			continue
-
-		} else {
-			fmt.Println("Error reading line: ", err)
-		}
+		cursorUp()
+		cursorUp()
+		fmt.Print("row2: Good Night!")
+		time.Sleep(1 * time.Second)
+		eraseInLine()
+		cursorToBeginThenDownBy(2)
+		println()
 	}
-
 }
